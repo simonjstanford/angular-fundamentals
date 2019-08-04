69d8728d3dd6791917f316716a1f20383691b81e
Simon Stanford <simonjstanford@gmail.com>
2019-08-04 10:50:59 +0100

    Added query parameters

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -2,51 +2,67 @@ import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search';
 import { HttpClient } from '@angular/common/http';
 import { GitUsers } from './git-users';
+import { NavigationExtras } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
-  cachedSearchValues: Array<{
-    [query: string]: GitSearch
-  }> = [];
-
-  cachedUserValues: Array<{
-    [query: string]: GitUsers
-  }> = [];
-
   constructor(private http: HttpClient) { }
 
-  gitSearch = (query: string, page: number): Promise<GitSearch> => {
+  gitSearch = (query: string, page: number, searchQueryParams:NavigationExtras): Promise<GitSearch> => {
     const promise = new Promise<GitSearch>((resolve, reject) => {
-      if (this.cachedSearchValues[query]) {
-        resolve(this.cachedSearchValues[query]);
-      } else {
-        this.http.get('https://api.github.com/search/repositories?q=' + query + '&page=' + page)
-        .toPromise()
-        .then((response) => {
-          resolve(response as GitSearch);
-        }, (error) => {
-          reject(error);
-        });
-      }
+    let searchString: string = this.buildSearchString(query, page, searchQueryParams);
+    this.http.get('https://api.github.com/search/repositories' + searchString)
+    .toPromise()
+    .then((response) => {
+      resolve(response as GitSearch);
+    }, (error) => {
+      reject(error);
+    });
+      
     });
     return promise;
   }
 
+  buildSearchString(query: string, page: number, params: NavigationExtras): string {
+    let searchString = '?q=' + query;
+
+    if (page) {
+      searchString += '&page=' + page;
+    }
+  
+    if (params) {
+      if (params.queryParams['language'] && params.queryParams['language'] !== '') {
+        searchString += '&language=' +  params.queryParams['language'];
+      }
+      if (params.queryParams['user'] && params.queryParams['user'] !== '') {
+        searchString += '&user=' +  params.queryParams['user'];
+      }
+      if (params.queryParams['size'] && params.queryParams['size'] !== '') {
+        searchString += '&size=' +  params.queryParams['size'];
+      }
+      if (params.queryParams['stars'] && params.queryParams['stars'] !== '') {
+        searchString += '&stars=' +  params.queryParams['stars'];
+      }
+      if (params.queryParams['topic'] && params.queryParams['topic'] !== '') {
+        searchString += '&topic=' +  params.queryParams['topic'];
+      }   
+    }
+
+    return searchString;
+  }
+  
+  
   gitSearchUsers = (query: string): Promise<GitUsers> => {
     const promise = new Promise<GitUsers>((resolve, reject) => {
-      if (this.cachedUserValues[query]) {
-        resolve(this.cachedUserValues[query]);
-      } else {
-        this.http.get('https://api.github.com/search/users?q=' + query)
-        .toPromise()
-        .then((repsonse) => {
-          resolve(repsonse as GitUsers);
-        }, (error) => {
-          reject(error);
-        });
-      }
+      this.http.get('https://api.github.com/search/users?q=' + query)
+      .toPromise()
+      .then((repsonse) => {
+        resolve(repsonse as GitUsers);
+      }, (error) => {
+        reject(error);
+      });
     });
     return promise;
   }
--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GitSearchService } from '../git-search.service';
 import { GitSearch } from '../git-search';
 import { GitUsers } from '../git-users';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router, NavigationExtras, Params } from '@angular/router';
 import { AdvancedSearchModel } from '../advanced-search-model'
 
 @Component({
@@ -17,6 +17,7 @@ export class GitSearchComponent implements OnInit {
   title: string;
   displayQuery: string;
   searchPage: number;
+  searchQueryParams:NavigationExtras;
 
   constructor(private gitSearchService: GitSearchService,
               private route: ActivatedRoute,
@@ -37,10 +38,24 @@ export class GitSearchComponent implements OnInit {
       this.searchPage = parseInt(params.get('page'), 10);
       this.gitSearch();
     });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.searchQueryParams['language'] = params['language'];
+      this.searchQueryParams['user'] = params['user'];
+      this.searchQueryParams['size'] = params['size'];
+      this.searchQueryParams['stars'] = params['stars'];
+      this.searchQueryParams['topic'] = params['topic'];
+      this.gitSearch();
+    });
+
+    //this is added so we can search for the same info twice
+    this.route.queryParamMap.subscribe((params) => {
+      this.gitSearch();
+    });
   }
 
   gitSearch = () => {
-    this.gitSearchService.gitSearch(this.searchQuery, this.searchPage).then((response) => {
+    this.gitSearchService.gitSearch(this.searchQuery, this.searchPage, this.searchQueryParams).then((response) => {
       this.searchResults = response;
     }, (error) => {
       alert('Error: ' + error.statusText);
@@ -57,24 +72,21 @@ export class GitSearchComponent implements OnInit {
 
   newQuery = () => {
     this.searchPage = 1;
-    let search: string = this.model.q;
-    let params: string = "";
-    this.modelKeys.forEach((element) => {
-      if (element == 'q') {
-        return false;
-      }
-      if (this.model[element]) {
-        params += '+' + element + ':' + this.model[element];
-      }
-    });
+    this.searchQuery = '';
 
-    if (search) {
-      this.searchQuery = search;
-    } 
-    
-    if (search && params !== '') {
-      this.searchQuery = search + '+' + params;
+    if (this.model.q) {
+      this.searchQuery = this.model.q;
     }
+
+    this.searchQueryParams = { 
+      queryParams: { 
+        'language': this.model.language,
+        'user': this.model.user,
+        'size': this.model.size,
+        'stars': this.model.stars,
+        'topic': this.model.topic
+      } 
+    };
 
     this.sendQuery();
   }
@@ -91,6 +103,6 @@ export class GitSearchComponent implements OnInit {
 
   sendQuery = () => {
     this.searchResults = null;
-    this.router.navigate(['/search/' + this.searchQuery + '/' + this.searchPage]);
+    this.router.navigate(['/search/' + this.searchQuery + '/' + this.searchPage], this.searchQueryParams );
   }
 }
