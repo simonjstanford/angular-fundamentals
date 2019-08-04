ad15514ab93bd95f4faa2fd7ee70fe22adf395f4
Simon Stanford <simonjstanford@gmail.com>
2019-08-04 20:04:11 +0100

    Fixed broken search string

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -13,7 +13,8 @@ export class GitSearchService {
   gitSearch = (query: string, page: number, searchQueryParams:NavigationExtras): Promise<GitSearch> => {
     const promise = new Promise<GitSearch>((resolve, reject) => {
     let searchString: string = this.buildSearchString(query, page, searchQueryParams);
-    this.http.get('https://api.github.com/search/repositories' + searchString)
+    let fullString = 'https://api.github.com/search/repositories' + searchString
+    this.http.get(fullString)
     .toPromise()
     .then((response) => {
       resolve(response as GitSearch);
@@ -27,27 +28,27 @@ export class GitSearchService {
 
   buildSearchString(query: string, page: number, params: NavigationExtras): string {
     let searchString = '?q=' + query;
-
-    if (page) {
-      searchString += '&page=' + page;
-    }
   
     if (params) {
       if (params.queryParams['language'] && params.queryParams['language'] !== '') {
-        searchString += '&language=' +  params.queryParams['language'];
+        searchString += '+language:' +  params.queryParams['language'];
       }
       if (params.queryParams['user'] && params.queryParams['user'] !== '') {
-        searchString += '&user=' +  params.queryParams['user'];
+        searchString += '+user:' +  params.queryParams['user'];
       }
       if (params.queryParams['size'] && params.queryParams['size'] !== '') {
-        searchString += '&size=' +  params.queryParams['size'];
+        searchString += '+size:' +  params.queryParams['size'];
       }
       if (params.queryParams['stars'] && params.queryParams['stars'] !== '') {
-        searchString += '&stars=' +  params.queryParams['stars'];
+        searchString += '+stars:' +  params.queryParams['stars'];
       }
       if (params.queryParams['topic'] && params.queryParams['topic'] !== '') {
-        searchString += '&topic=' +  params.queryParams['topic'];
+        searchString += '+topic:' +  params.queryParams['topic'];
       }   
+    }
+
+    if (page) {
+      searchString += '&page=' + page;
     }
 
     return searchString;
