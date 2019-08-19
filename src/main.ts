33f38e150fcdddbdd5fa39032758dfb196550ba0
Simon Stanford <simonjstanford@gmail.com>
2019-08-19 13:38:06 +0100

    Set up production build

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+else {
+  alert("App is running in development mode.");
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
